71d0c07322cfca84d6a4fc150e9670f65d7803bd
Hugo Filipe Paulino Santos <hugo.paulino.santos@gmail.com>
2022-11-13 14:59:20 +0000

    Add awnsers and add docstrings
    Signed-off-by: Hugo Filipe Paulino Santos <hugo.paulino.santos@gmail.com>

--- README.md
+++ README.md
@@ -4,20 +4,62 @@ This repo contains two tasks, product analysis and product classification. The t
 
 ## How to run the product analysis
 
-In order to run the product analysis you need to have docker installed, after installed go the the product_analysys folder and run
+In order to run the product analysis you need to have docker installed, after installed go the the product_analysis folder and run
 
-docker compose up 
+`docker compose up`
 
 This command will:
+
 1. Create a container with Postgres sql database.
 2. Create a table products, and fill the table with the dataset testset_B.tsv that can be found in **data** folder.
 3. Create a MongoDB no sql database.
 4. You can check the postgres database with adminer that will run in port 8080. With that tool you can connect to the postgres database and check the table creation and the table insertion.
 
-To run the python script
+Run the python script to get the results:
 
-Expected results:
--- missing part yet
+**Create a virtual environment:**
+
+```
+python -m venv venv 
+source venv/bin/activate 
+pip install -r requirements.txt
+```
+**After environment creation run code:**
+
+`python app.py`
+
+**Results**
+
+You can check the results connected when connected to MongoDB using Compass.
+
+3a.
+| price      | pricerank |
+| ----------- | ----------- |
+| 14900      | 1       |
+| 15900	     | 2        |
+|16900|	3|
+|17900|	4|
+|18900|	5|
+|19900|	6|
+|20900|	7|
+|21900|	8|
+|22900|	9|
+|23900|	10|
+|23900|	10|
+|23900|	10|
+| ...||
+
+3b. 
+| min_hdd_gb      | max_hdd_gb |
+| ----------- | ----------- |
+| 500GB      | 2000 GB       |
+
+3c. 
+| median_ghz      | ram_gb |
+| ----------- | ----------- |
+| 2.5474382433668801      | 4       |
+| 2.5389019239793524   | 8        |
+| 2.5558011049723757   | 16        |
 
 ## How to run the product analysis
 
@@ -27,13 +69,14 @@ After I did multiple experiments in the training the model I've save it as pickl
 Then a Rest api using FastAPI framework was created with a unit test to test the endpoint.
 
 
-In order to run the code we have two ways, one is to create a virtual environment and run our aplication :
+In order to run the code we have two ways, one is to create a virtual environment and run our application :
 
 
-`python -m venv venv`
-`source venv/bin/activate`
-`pip install -r requirements.txt`
-
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
 
 The other is using a container:
 
@@ -42,7 +85,8 @@ on the folder product classification do:
 `docker compose up -d`
 
 This will initiate the product classification api,we can test it in :
-localhost:8085/docs
+
+`localhost:8085/docs`
 
 
 
--- product_analysis/app/app.py
+++ product_analysis/app/app.py
@@ -2,12 +2,39 @@
 from pymongo import MongoClient
 import psycopg2
 import collections
-import logging
 
-class database(object):
+class Database():
+    """
+    A class to represent a database with respective connections and queries.
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    price_rank_grouped_by_brand(self):
+        Gets product data from Posgresdb and execute the ranking 
+        based on price grouped by brand
+    min_hdd_gb(self):
+        Gets product data from Posgresdb and executes the min of hdd_gb column
+    max_hdd_gb(self):
+        Gets product data from Posgresdb and executes the max of hdd_gb column
+    ghz_median_grouped_by_ramgb(self):
+        Gets product data from Posgresdb and execute the median of the ghz 
+        grouped by ram_gb
+        """
+
+
     def __init__(self):
         # Connect to existing database
         #TODO change this to a ini file a load it with config parser
+
+        """
+        Constructs all the necessary attributes for the database object.
+
+        Parameters
+        ----------
+        """
         self.sql_conn = psycopg2.connect(
             database="productdb",
             user="docker",
@@ -16,15 +43,27 @@ class database(object):
             port="5432"
         )
         mongo_client = MongoClient(host ="mongodb:27017", username='root', password='root')
-        mydb = mongo_client["productskpis_db"]
-        self.collection = mydb["productkpis_results"]    
+        self.mydb = mongo_client["productskpis_db"]
+        #self.collection = mydb["productkpis_results"]    
 
 
     def price_rank_grouped_by_brand(self):
+        """
+        Gets data from product table and preform a ranking query based on price grouped by brand
+        Inserts the results on mongodb
+
+        Parameters
+        ----------
+        self : object 
+            Constructor object containing db.
+
+        Returns
+        -------
+        None
+        """
         cur = self.sql_conn.cursor()
 
-        # Query the database 
-        cur.execute("SELECT price, RANK() OVER ( PARTITION BY brand ORDER BY price DESC ) pricerank FROM product GROUP BY brand, price")
+        cur.execute("SELECT price, RANK() OVER ( ORDER BY price ASC ) pricerank FROM product GROUP BY brand, price")
         rows = cur.fetchall()
 
         docArray = []
@@ -33,50 +72,72 @@ class database(object):
             doc['price'] = row[0]
             doc['rank'] = row[1]
             docArray.append(doc)
-            #print(row)
-        self.collection.insert_many(docArray)
-        
-        print("Price Rank grouped by brand")
-
+        collection = self.mydb["productkpis_results_3a"]
+        collection.insert_many(docArray)
 
     def min_hdd_gb(self):
-        
+        """
+        Gets data from product table and preform a min query based on hdd_gb column.
+        Inserts the results on mongodb
+
+        Parameters
+        ----------
+        self : object 
+            Constructor object containing db connections.
+
+        Returns
+        -------
+        None
+        """
         cur = self.sql_conn.cursor()
 
         # Query the database 
-        docArray = []
         cur.execute("SELECT MIN(hdd_gb) FROM product")
         rows = cur.fetchone()
-        print(rows)
+
         doc = collections.OrderedDict()
-        doc['max_price'] = rows[0]
-        #docArray.append(doc)
-        
-        self.collection.insert_one(doc)
-        #for row in rows:
-        #    print(row)
-        print("Max hdd_gb")
+        doc['min_hdd_gb'] = rows[0]
+        collection = self.mydb["productkpis_results_3b1"]
+        collection.insert_one(doc)
 
     def max_hdd_gb(self):
-        
+        """
+        Gets data from product table and preform a max query based on hdd_gb column.
+        Inserts the results on mongodb
+
+        Parameters
+        ----------
+        self : object 
+            Constructor object containing db connections.
+
+        Returns
+        -------
+        None
+        """
         cur = self.sql_conn.cursor()
 
         # Query the database 
-        docArray = []
         cur.execute("SELECT MAX(hdd_gb) FROM product")
         rows = cur.fetchone()
         doc = collections.OrderedDict()
-        doc['max_price'] = rows[0]
-        #docArray.append(doc)
-        
-        self.collection.insert_one(doc)
-        #for row in rows:
-        #    print(row)
-
-        print("Max hdd_gb")
+        doc['max_hdd_gb'] = rows[0]
+        collection = self.mydb["productkpis_results_3b2"]
+        collection.insert_one(doc)
 
     def ghz_median_grouped_by_ramgb(self):
-        
+        """
+        Gets data from product table and preform a median query based on ghz column grouped by ramgb.
+        Inserts the results on mongodb
+
+        Parameters
+        ----------
+       self : object 
+            Constructor object containing db connections.
+
+        Returns
+        -------
+        None
+        """
         cur = self.sql_conn.cursor()
 
         # Query the database  
@@ -86,19 +147,25 @@ class database(object):
         docArray = []
         for row in rows:
             doc = collections.OrderedDict()
-            doc['median_ghz'] = row[0]
+            doc['median_ghz'] = str(row[0])
             doc['ram_gb'] = row[1]
             docArray.append(doc)
-
-        self.collection.insert_many(docArray)
-        
-        print("Max hdd_gb")
-
-
+        collection = self.mydb["productkpis_results_3c"]
+        collection.insert_many(docArray)
 
     def main(self):
+        """
+        Executes all the query and insert functions sequentialy.
 
+        Parameters
+        ----------
+        self : object 
+            Constructor object containing db connections.
 
+        Returns
+        -------
+        None
+        """
         # Response question 3a
         self.price_rank_grouped_by_brand()
 
@@ -109,28 +176,15 @@ class database(object):
         # Response question 3c
         self.ghz_median_grouped_by_ramgb()
 
-        # Close communications with database
-        self.cur.close()
-        self.conn.close()
-        # Open cursor to perform database operation
-        #cur = self.conn.cursor()
-
-
-        # Query the database 
-        #cur.execute("SELECT * FROM product limit 1")
-        #rows = cur.fetchall()
-        #for row in rows:
-        #    print(row)
-
-        # Query the database 
-        #cur.execute("SELECT RANK() OVER(PARTITION BY a.price ORDER BY a.brand) AS rnk")
-        #rows = cur.fetchall()
-        #for row in rows:
-        #    print(row)
-
+        # Close communication with database
+        self.sql_conn.close()
     
 if __name__ == "__main__":
     print("Aplication startup")
 
+    #initiate db connections
+    database_obj = Database()
 
-    database().main()
+    # perform queries and save the results in mongodb
+    database_obj.main()
+    
